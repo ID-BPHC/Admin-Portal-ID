70b87776f1ccfcde82d05a1eba450035c32d3e75
Sohail Rajdev <sohailrajdev97@gmail.com>
2017-06-06 21:50:42 +0530

    Added a footer and php pages corresponding to navbar links

--- mid-sem-feedback-config.php
+++ mid-sem-feedback-config.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"><!-- InstanceBegin template="/Templates/LoggedIn-Template.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- TemplateBeginEditable name="doctitle" -->
+<!-- InstanceBeginEditable name="doctitle" -->
 <title>Template</title>
-<!-- TemplateEndEditable -->
+<!-- InstanceEndEditable -->
 <!-- Bootstrap -->
-<link rel="stylesheet" href="../css/bootstrap.css">
-<link href="../css/main.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link href="css/main.css" rel="stylesheet" type="text/css">
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-<!-- TemplateBeginEditable name="head" -->
-<!-- TemplateEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
 </head>
 
 <body style="padding-top: 50px">
@@ -51,14 +51,14 @@
   <!-- /.container-fluid -->
 </nav>
 <div class="container-fluid">
-  <div class="row" id="hero"> <img src="../images/Logo.png" alt="Placeholder image" class="img-responsive" id="herologo">
+  <div class="row" id="hero"> <img src="images/Logo.png" alt="Placeholder image" class="img-responsive" id="herologo">
     <h1 id="heroheading">Administartion Portal</h1>
-    <img src="../images/hero_img.jpeg" alt="Placeholder image" class="img-responsive" id="heroimg"></div>
+    <img src="images/hero_img.jpeg" alt="Placeholder image" class="img-responsive" id="heroimg"></div>
   <div class="row">
-    <div class="col-md-12 col-lg-10 col-lg-offset-1"><!-- TemplateBeginEditable name="EditRegion3" -->
+    <div class="col-md-12 col-lg-10 col-lg-offset-1"><!-- InstanceBeginEditable name="EditRegion3" -->
       <h1>Heading</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet doloremque cum maxime architecto veritatis id omnis magni dignissimos sint eius, ea, iure praesentium aspernatur error incidunt, quos harum blanditiis quo.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit quidem possimus molestiae rerum incidunt iste, consequatur laborum ullam accusantium cumque in, voluptas porro autem repudiandae ipsum, officiis fugiat debitis voluptatem.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quaerat, quo unde quod eos delectus cupiditate eligendi quia iusto repudiandae corporis dignissimos optio! Numquam dicta vero, corporis quis, recusandae doloribus?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat enim suscipit, ullam minus. Sapiente qui quas, cupiditate quo quos libero quidem? Nobis, earum, laudantium minus incidunt natus expedita placeat non.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque optio veritatis quam iure rem culpa sequi beatae natus ab dolorum, temporibus, alias in nemo vel quaerat aut ullam quas officiis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, quisquam consequatur esse quasi perspiciatis inventore velit saepe dolore hic aliquid tempora nisi sunt deleniti, iusto itaque dicta omnis earum. Accusamus.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sunt facere praesentium corporis odio vitae ab fugiat ut, earum nisi, natus, error incidunt cupiditate fugit minima quae doloribus repudiandae obcaecati.</p>
-    <!-- TemplateEndEditable --></div>
+    <!-- InstanceEndEditable --></div>
   </div>
   <div class="row">
     <div class="col-md-12">
@@ -67,8 +67,8 @@
   </div>
 </div>
 <!-- /.container-fluid --><!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-<script src="../js/jquery-1.11.3.min.js"></script> 
+<script src="js/jquery-1.11.3.min.js"></script> 
 <!-- Include all compiled plugins (below), or include individual files as needed --> 
-<script src="../js/bootstrap.js"></script>
+<script src="js/bootstrap.js"></script>
 </body>
-</html>
+<!-- InstanceEnd --></html>
